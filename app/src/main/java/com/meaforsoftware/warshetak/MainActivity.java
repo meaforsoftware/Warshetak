9b7f0ca3339ef825887a73d69e4c13d11f911071
Eslam <eslam.abdalla93@hmail.com>
2018-11-01 16:38:19 +0200

    shazly

--- app/src/main/java/com/meaforsoftware/warshetak/MainActivity.java
+++ app/src/main/java/com/meaforsoftware/warshetak/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test push errcd or
+        // test push errcd or gfbhghgfhgfhg
 
     }
 }
