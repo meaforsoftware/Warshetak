85dc67c94becb5097b992a0da97b9feee9cfd070
MohamedHElshazly <mohammed.hassan.farouk@gmail.com>
2018-11-06 09:28:32 +0200

    test shazly push

--- app/src/main/java/com/meaforsoftware/warshetak/MainActivity.java
+++ app/src/main/java/com/meaforsoftware/warshetak/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // test push errcd or gfbhghgfhgfhg
+        // test push shazly
 
     }
 }
